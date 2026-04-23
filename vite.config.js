ae3abc4f57be96ca4247c2880c99f4a4364f4577
migueel15-bb <miguelbailachbadia@gmail.com>
2026-04-23 10:03:15 +0200

    Funcion de guardado de correos

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //la ruta base se establece en './' para que el proyecto pueda ser desplegado correctamente en cualquier servidor, ya que esto indica que los recursos se cargarán desde la raíz del proyecto añadiendo /dist.
+  base: './',
 })
